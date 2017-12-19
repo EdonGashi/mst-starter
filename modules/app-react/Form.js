d2890ab33e5641fb0171a9fbce9e56ad195a296e
EdonGashi <edongashi@hotmail.co.uk>
2017-12-19 05:00:04 +0100

    Testing Form and qs

---
+++ modules/app-react/Form.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import invariant from 'utils/invariant'
+import qs from 'utils/qs'
+
+const arrayFrom = Array.from || (obj => [].slice.call(obj))
+function getFormElements(form) {
+  const result = []
+  if (!form) {
+    return []
+  }
+
+  const elements = arrayFrom(form.elements)
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i]
+    if (!element.name || element.disabled) {
+      continue
+    }
+
+    if (element.type === 'file') {
+      const files = element.files
+      for (let j = 0; j < files.length; j++) {
+        result.push({ name: element.name, value: files[j] })
+      }
+    } else if (element.type === 'select-multiple' || element.type === 'select-one') {
+      const opts = arrayFrom(element.options)
+      for (let j = 0; j < opts.length; j++) {
+        const opt = opts[j]
+        opt.selected && result.push({ name: element.name, value: opt.value })
+      }
+    } else if (element.type === 'checkbox' || element.type === 'radio') {
+      if (element.checked) {
+        result.push({ name: element.name, value: element.value })
+      }
+    } else {
+      result.push({ name: element.name, value: element.value })
+    }
+  }
+
+  return result
+}
+
+export default class Form extends React.Component {
+  static propTypes = {
+    children: propTypes.node,
+    params: propTypes.any
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(new )
+  }
+
+  render() {
+    const { app } = this.context
+    invariant(app && app.router, 'Form must be rendered inside an application context with a valid router.')
+    return <form /* TODO: add href */ method='post' onSubmit={this.handleSubmit}>
+      <input type='hidden' suppressHydrationWarning={true} name='__state__' />
+      {this.props.children}
+    </form>
+  }
+}
