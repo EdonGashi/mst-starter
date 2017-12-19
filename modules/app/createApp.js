01e07829f6f184dbaad6414138524915d60e9f2e
EdonGashi <edongashi@hotmail.co.uk>
2017-12-19 15:12:35 +0100

    made app creation delayed

--- modules/app/createApp.js
+++ modules/app/createApp.js
@@ -1,7 +1,9 @@
 import { AppNode } from './tree'
 
-export default function createApp(payload, ...initializers) {
-  const app = new AppNode(null, payload)
-  initializers.forEach(initializer => initializer(app))
-  return app
+export default function createApp(...initializers) {
+  return function app(payload) {
+    const root = new AppNode(null, payload)
+    initializers.forEach(initializer => initializer(app))
+    return root
+  }
 }
